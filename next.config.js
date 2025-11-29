42f7118bbe6ef5704ae3dcfa6644eee422a2e5f2
Earnwithalee7890 <earnwithalee7890@gmail.com>
2025-11-29 10:43:21 +0500

    Fix Vercel build: mark pino and thread-stream as external packages

--- next.config.js
+++ next.config.js
@@ -1,31 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        // Exclude test files and dev dependencies from being bundled
-        config.module = config.module || {};
-        config.module.rules = config.module.rules || [];
+    // Mark packages as external to prevent bundling issues
+    serverComponentsExternalPackages: [
+        'pino',
+        'thread-stream',
+        '@walletconnect/ethereum-provider'
+    ],
 
+    webpack: (config, { isServer }) => {
+        // Exclude test files from being processed
         config.module.rules.push({
-            test: /node_modules\/(thread-stream|pino)\/test\//,
-            use: 'null-loader',
+            test: /\/(test|tests|__tests__|bench)\//,
+            loader: 'ignore-loader'
         });
 
-        // Ignore problematic files
-        config.resolve = config.resolve || {};
-        config.resolve.alias = config.resolve.alias || {};
+        // Ignore specific problematic files
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'thread-stream/test': false,
+            'pino/test': false,
+        };
 
         return config;
-    },
-    // Exclude test files from being processed
-    experimental: {
-        turbo: {
-            rules: {
-                '*.test.{js,mjs,ts}': {
-                    loaders: [],
-                    as: '*.js',
-                },
-            },
-        },
     },
 };
 
